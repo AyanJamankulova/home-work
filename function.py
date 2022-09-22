72c98f51a84b26f87cb7d89729b6879159fa6b46
AyanJamankulova <jamankulova.ayana283@gmail.com>
2022-09-22 17:57:37 +0600

    first commit

---
+++ function.py
@@ -0,0 +1,49 @@
+# Задача1
+# def add(sideA,sideB):
+#     perimetr = (sideA*2 + sideB*2)
+#     print("Периметр прямоугольника:", perimetr)    
+    
+# def sun (sideA,sideB):
+#     area = (sideA*sideB)
+#     print("Площадь прямоугольника:", area)
+
+# Задача2
+# lst = [1,2,3,4,5,6,7,8,9,10]
+
+
+# Задача3
+# new_menu = {'beefstrogonoff': 400,'burger': 250,'meatloaf': 550,'chicken pot pie': 450, 'beefshteks': 700}
+
+
+#Задача4
+# def sec(h,m,s):
+#     print(h*3600+m*60+s)
+# # sec()
+
+
+#Задача5
+# def chat_bot():
+#     while True:
+#         word = input("Напишите слово:")
+#         if word =="":
+#             print("Как классно когда ты молчишь.Продолжай в том же духе!")
+#         elif word == "ВОТ ТАК!":
+#             print("Успокойся")
+#         elif "?" in word:
+#             print("Конечно")
+#         else:
+#             print(" ну и что")
+            
+# Задача6
+# def short_split(word):
+#     word_split = word.split()
+#     new_lst = []
+#     for a in word_split:
+#         new_lst.append(a.title()[0])
+#     res = "".join(new_lst)
+#     print(res)
+
+# short_split("Кыргызская Республика")
+# short_split("Жаманкулова Аяна")
+# short_split(" For you interest")
+        
---
+++ main1.py
@@ -0,0 +1,11 @@
+#Задача1
+from function import add, sun, lst,new_menu,short_split,sec,chat_bot
+add(2,4)
+sun(3,6)
+print(lst[::-1])
+print(new_menu)
+short_split("")
+sec(20,35,53)
+chat_bot()
+
+
